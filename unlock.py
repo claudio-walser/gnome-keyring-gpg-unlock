abf0a929a81322dd95ca759c14101bc7d09fbee0
Claudio <info@gitcd.io>
2023-01-29 21:25:09 +0100

    Simpler as well

--- unlock.py
+++ unlock.py
@@ -5,9 +5,8 @@ import sys
 from src.Secret import Secret
 from src.Keyring import Keyring
 
-publicKey = 'E51D8637C4F97C850BC97BF55638393858B04300'
 
-secret = Secret(publicKey)
+secret = Secret()
 keyringPassword = secret.decrypt('/home/claudio/.sec/gnome-keyring')
 
 keyring = Keyring()
